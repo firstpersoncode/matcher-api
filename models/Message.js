c779a5818b6401da4a254864bdd0146b6f4bc551
firstpersoncode <fpcdatadriver@gmail.com>
2023-03-06 17:21:44 +0700

    adjust message model, remove pinned field, add timestamp field

--- models/Message.js
+++ models/Message.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  type: {
-    type: String,
-    enum: ["chat", "announcement"],
-    required: false,
-    default: "chat",
+const MessageSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    type: {
+      type: String,
+      enum: ["chat", "announcement"],
+      required: false,
+      default: "chat",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Participant",
+      required: true,
+    },
+    match: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Match",
+      required: true,
+    },
   },
-  pinned: { type: Boolean, required: false, default: false },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Participant",
-    required: true,
-  },
-  match: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Match",
-    required: true,
-  },
-
-  created: { type: Date, required: false, default: new Date() },
-});
+  { timestamps: true }
+);
 
 MessageSchema.index({ created: -1 });
 
