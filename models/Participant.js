e007bcaaed6b9a0440c98d2b42601a341c815de8
firstpersoncode <fpcdatadriver@gmail.com>
2023-03-06 17:22:22 +0700

    timestamp field

--- models/Participant.js
+++ models/Participant.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const ParticipantSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  location: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: false,
-      default: "Point",
+const ParticipantSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: false,
+        default: "Point",
+      },
+      coordinates: { type: [Number], required: false, default: [0, 0] },
     },
-    coordinates: { type: [Number], required: false, default: [0, 0] },
   },
-});
+  { timestamps: true }
+);
 
 ParticipantSchema.index({ email: 1 }, { unique: true });
 ParticipantSchema.index({ location: "2dsphere" });
